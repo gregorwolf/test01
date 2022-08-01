d912b4783ec30fe8d91828aa50897aff17005f14
lemaiwo <wouterlemaire120@hotmail.com>
2022-08-01 22:26:12 +0200

    fix reload fix html error

--- app/functions/webapp/ext/controller/LRExtend.controller.js
+++ app/functions/webapp/ext/controller/LRExtend.controller.js
@@ -8,12 +8,13 @@ sap.ui.define(
           
           },
           onViewNeedsRefresh: function () {  
-            const holder = document.getElementById("functions::FunctionsList--fe::CustomTab::tableView1--holder");
+            // const holder = document.getElementById("functions::FunctionsList--fe::CustomTab::tableView1--holder");
+            const holderControl = this.base.byId(this.getView().getId() + "--fe::CustomTab::tableView1--holder");
             const router = null; //this.base.getRouter();
-            if (!this._diagram) {
+            // if (!this._diagram) {
               this._diagram = new Diagram(router);
-              this._diagram.createDiagram(holder);
-            }
+              this._diagram.createDiagram(holderControl.getDomRef());
+            // }
           },
         },
       });
